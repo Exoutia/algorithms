b988868931ae9f25d5bdce8375a334957ddcf362
Exoutia <jhav365@gmail.com>
2022-06-28 10:13:37 +0530

    done

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,11 @@
+def bub_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+x = [7,8,9,1,2,3,4,76,5,6]
+bub_sort(x)
+print (x)
